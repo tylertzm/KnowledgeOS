2f50d170251ad17d127a1c30e08ba432a84fa8fb
Tyler <TZM2002@protonmail.com>
2025-05-29 21:16:28 +0200

    analytics

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyles } from './styles/GlobalStyles';
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from '@vercel/analytics/react'; // Correct import for React
 
 
 const root = ReactDOM.createRoot(
