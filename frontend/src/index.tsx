795bd0f977bfce3e2c89f6bbcb8d30f6bd67aa7c
Tyler <TZM2002@protonmail.com>
2025-05-29 21:12:11 +0200

    warning

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
 import { GlobalStyles } from './styles/GlobalStyles';
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/next"
 
 
 const root = ReactDOM.createRoot(
